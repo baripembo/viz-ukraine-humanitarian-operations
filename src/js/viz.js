f9f592ebb3a3404b68f603f1f84ce53f9d75817e
baripembo <erikawei@gmail.com>
2020-11-06 15:16:19 -0500

    fix merge conflict

--- src/js/viz.js
+++ src/js/viz.js
@@ -67,7 +67,8 @@ $( document ).ready(function() {
   function getData() {
     console.log('Loading data...')
     Promise.all([
-      d3.json('https://raw.githubusercontent.com/OCHA-DAP/hdx-scraper-covid-viz/master/out.json'),
+      d3.json('https://raw.githubusercontent.com/OCHA-DAP/hdx-scraper-covid-viz/access/out.json'),
+      //d3.json('https://raw.githubusercontent.com/OCHA-DAP/hdx-scraper-covid-viz/master/out.json'),
       d3.json('data/ocha-regions-bbox.geojson')
     ]).then(function(data) {
       console.log('Data loaded');
