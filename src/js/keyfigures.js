3e995d33cb1b90e2fd7fa6c6458171472734ac93
baripembo <erikawei@gmail.com>
2021-03-24 16:48:55 -0400

    removing COVID-19 from all CPBF and CERF titles

--- src/js/keyfigures.js
+++ src/js/keyfigures.js
@@ -109,7 +109,7 @@ function setKeyFigures() {
 	//CERF
 	else if (currentIndicator.id=='#value+cerf+covid+funding+total+usd') {
 		createKeyFigure('.figures', 'Number of Countries', '', totalCountries);
-		createKeyFigure('.figures', 'Total CERF COVID-19 Funding', '', formatValue(data['#value+cerf+covid+funding+total+usd']));
+		createKeyFigure('.figures', 'Total CERF Funding', '', formatValue(data['#value+cerf+covid+funding+total+usd']));
 		if (data['#value+cerf+covid+funding+total+usd'] > 0) {
 			var gmText = getGamText(data, 'cerf');
 			$('.figures .key-figure .inner').append('<div class="small">'+ gmText +'</div>');
@@ -119,7 +119,7 @@ function setKeyFigures() {
 	else if (currentIndicator.id=='#value+cbpf+covid+funding+total+usd') {
 		//num countries
 		createKeyFigure('.figures', 'Number of Countries', '', totalCountries);
-		createKeyFigure('.figures', 'Total CBPF COVID-19 Funding', '', formatValue(data['#value+cbpf+covid+funding+total+usd']));
+		createKeyFigure('.figures', 'Total CBPF Funding', '', formatValue(data['#value+cbpf+covid+funding+total+usd']));
 		
 		//gam
 		if (data['#value+cbpf+covid+funding+total+usd'] > 0) {
