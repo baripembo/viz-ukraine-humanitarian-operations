bae22a09865eb9c910928985941e98da82fc0ec2
baripembo <erikawei@gmail.com>
2022-05-03 13:32:08 -0400

    add deep links to ukr view

--- src/js/map.js
+++ src/js/map.js
@@ -116,15 +116,35 @@ function displayMap() {
 }
 
 function deepLinkView() {
-  var countryCode = 'UKR';
+  let countryCode = 'UKR';
   if (countryCodeList.hasOwnProperty(countryCode)) {
     currentCountry.code = countryCode;
     currentCountry.name = 'Ukraine';
 
     //find matched features and zoom to country
-    var selectedFeatures = matchMapFeatures(currentCountry.code);
+    let selectedFeatures = matchMapFeatures(currentCountry.code);
     selectCountry(selectedFeatures);
   }
+
+  //deep link to specific layer 
+  let location = window.location.search;
+  if (location.indexOf('?layer=')>-1) {
+    let param = location.split('layer=')[1];
+    let layer = $('.map-legend.country').find('input[data-layer="'+param+'"]');
+    selectLayer(layer);
+  }
+}
+
+function selectLayer(layer) {
+  layer.prop('checked', true);
+  currentCountryIndicator = {id: layer.val(), name: layer.parent().text()};
+  updateCountryLayer();
+  vizTrack(`main ${currentCountry.code} view`, currentCountryIndicator.name);
+
+  //reset any deep links
+  let layerID = layer.attr('data-layer');
+  let location = (layerID==undefined) ? window.location.pathname : window.location.pathname+'?layer='+layerID;
+  window.history.replaceState(null, null, location);
 }
 
 function matchMapFeatures(country_code) {
@@ -142,9 +162,7 @@ function createEvents() {
   //country legend radio events
   $('input[type="radio"]').click(function(){
     var selected = $('input[name="countryIndicators"]:checked');
-    currentCountryIndicator = {id: selected.val(), name: selected.parent().text()};
-    updateCountryLayer();
-    vizTrack(`main ${currentCountry.code} view`, currentCountryIndicator.name);
+    selectLayer(selected);
   });
 
   //chart view trendseries select event
